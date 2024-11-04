edd76d87269c4453f77b89daedfa15168a43a3ac
Victor Hugo Alves Pinheiro <victorpinheiro1008@gmail.com>
2024-11-04 08:27:21 -0300

    back

--- conexao.php
+++ conexao.php
@@ -1,12 +1,14 @@
 <?php
+    #Criação das variaveis para login no banco de dados#
     $Hostname = "Localhost";
     $BD= "Catavento";
     $usuario = "Teste";
     $senha = "Teste";
-    #Criação das variaveis para login no banco de dados#
+    #Conexão com o banco de dados#
     try{
         $pdo = new pdo("mysql:host=$Hostname;dbname=$BD",$usuario,$senha);
     }catch(PDOexception $e){
-        echo $e-> getmessage(); 
+        $e->getmessage();
+    #buscar possiveis mensagens de erros no BD#
     }
 ?>
